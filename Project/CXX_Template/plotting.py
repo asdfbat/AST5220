a80a3d8759c9764652dc424c26c1d23f7043f45d
asdfbat <jonas.s.lunde@outlook.com>
2020-01-31 13:34:09 +0100

    Added more plotting

--- Project/CXX_Template/plotting.py
+++ Project/CXX_Template/plotting.py
@@ -8,31 +8,47 @@ data = np.loadtxt("../data/cosmology.txt")
 print(data.shape)
 
 x = data[:,0]
-eta_of_x = data[:,1]#/Constants.Mpc
-Hp_of_x = data[:,2]
+eta_of_x = data[:,1]/Constants.Mpc/1000  # Eta(x) in Gpc
+Hp_of_x = data[:,2]/1000*Constants.Mpc
+H_of_x = Hp_of_x/np.exp(x)
 OmegaB = data[:,4]
 OmegaCDM = data[:,5]
 OmegaLambda = data[:,6]
 OmegaR = data[:,7]
 
+a = np.exp(x)
+z = 1/a - 1
 
-# plt.figure(figsize=(20,14))
-# plt.plot(x, OmegaB, lw=4, label="OmegaB")
-# plt.plot(x, OmegaCDM, lw=4, label="OmegaCDM")
-# plt.plot(x, OmegaR, lw=4, label="OmegaR")
-# plt.plot(x, OmegaLambda, lw=4, label="OmegaLambda")
-# plt.plot(x, OmegaB + OmegaCDM + OmegaR + OmegaLambda, lw=4, label="Total")
-# plt.legend()
-# plt.show()
+print(z[-2])
+print(H_of_x[-1])
 
-plt.figure(figsize=(20,14))
-plt.plot(x, eta_of_x, lw=4)
-plt.xlabel("x")
-plt.ylabel("eta(x)")
+fig, ax = plt.subplots(figsize=(20,14))
+ax.plot(x, OmegaB, lw=4, label="OmegaB")
+ax.plot(x, OmegaCDM, lw=4, label="OmegaCDM")
+ax.plot(x, OmegaR, lw=4, label="OmegaR")
+ax.plot(x, OmegaLambda, lw=4, label="OmegaLambda")
+ax.plot(x, OmegaB + OmegaCDM + OmegaR + OmegaLambda, lw=4, label="Total")
+ax.fill_between(x, 0, 1, OmegaR>(OmegaCDM+OmegaB), alpha=0.2)
+ax.fill_between(x, 0, 1, (OmegaR<(OmegaCDM+OmegaB) * ((OmegaCDM+OmegaB) > OmegaLambda)), alpha=0.2)
+ax.fill_between(x, 0, 1, (OmegaCDM+OmegaB) < OmegaLambda, alpha=0.2)
+plt.legend()
 plt.show()
 
-plt.figure(figsize=(20,14))
-plt.plot(x, Hp_of_x, lw=4)
-plt.xlabel("x")
-plt.ylabel("Hp(x)")
+fig, ax = plt.subplots(2, 2, figsize=(24, 18))
+ax[0,0].plot(x, eta_of_x, lw=4)
+ax[0,0].set_xlabel("x")
+ax[0,0].set_ylabel("eta(x) [Gpc]")
+
+ax[0,1].plot(x, Hp_of_x, lw=4)
+ax[0,1].set_xlabel("x")
+ax[0,1].set_ylabel("Hp(x)")
+
+ax[1,0].semilogy(x, H_of_x, lw=4)
+ax[1,0].set_xlabel("x")
+ax[1,0].set_ylabel("H(x)")
+
+ax[1,1].loglog(z, H_of_x, lw=4)
+ax[1,1].set_xlim(1.5*z[0], 0.01)
+ax[1,1].set_xlabel("z")
+ax[1,1].set_ylabel("H(z)")
 plt.show()
