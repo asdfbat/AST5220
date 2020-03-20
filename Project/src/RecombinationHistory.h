4c3ccd83b470e51684575f483f3b48184ee6cefd
jonas <jonas.s.lunde@outlook.com>
2020-03-20 13:57:06 +0100

    woop

--- Project/src/RecombinationHistory.h
+++ Project/src/RecombinationHistory.h
@@ -23,7 +23,7 @@ class RecombinationHistory{
     const double x_end    = 4;
     
     // Numbers of points of Xe,ne array (modify as you see fit)
-    const int npts_rec_arrays = 5e4;
+    const int npts_rec_arrays = 1e5;
   
     // Xe for when to switch between Saha and Peebles
     double Xe_saha_limit = 0.99;
